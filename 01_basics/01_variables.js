10dc99b611b8130b9467e43b60740570d442a0c3
Anil Singh <52264327+anilsuyal@users.noreply.github.com>
2023-10-15 17:36:41 +0000

    variables and datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,19 @@ const accountId = 144553;
 let accountEmail = "anil@google.com";
 var accountPassword = 12345;
 accountCity = "Jaipur";
+let accountState;
 
-accountId = 2;
+//accountId = 2; // not allowed
 
-console.log(accountId);
+// console.log(accountId, accountEmail, accountPassword, accountCity); 
+
+accountEmail = "anil@coding.com";
+accountPassword = 224647;
+accountCity = "Dehradun"; // not a good and recommended way
+
+/*
+Prefer not to use var
+because of the issue in block scope and functional scope.
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all the JS code as newer version
+
+// alert( 3+3 ); // Can't use inside the nodejs, because it can be done inside the browser's engine only
+
+
+let name = "Anil" // String datatype
+let age = 22 // Number
+let isLoggedIn = false // Boolean
+let state;
+
+// Primitive datatypes 
+
+// number => 2 to the power 53
+// bigint => large integers
+// string = ""
+// boolean = true/false
+// null => object/standalone value which can be assigned to a variable
+// undefined => value which defines a variable which is created but not initilized
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Anil") // string
+console.log(typeof age) // number
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
