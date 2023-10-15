3f55e5da1773787798a95b1851a07ccc10e232c3
Anil Singh <52264327+anilsuyal@users.noreply.github.com>
2023-10-15 18:50:59 +0000

    Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,36 +1,90 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
-
-
-// "33" => 33 // type: number
-// "33abc" => NaN(not a number) // type: number
-// true => 1; false => 0 // type: number 
-// null => 0 // type: object
-// undefined => NaN // type: undefined
-
-let isLoggedIn = 1
-
-let booleanLoggedIn = Boolean(isLoggedIn)
-
-console.log(typeof booleanLoggedIn) 
-
-console.log(booleanLoggedIn)
-
-// 1 or non zero number => true; 0 => false // type: boolean
-// "" => false // type: boolean
-// "Anil" => true // type:boolean 
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
-let someNumber = 33
+// // "33" => 33 // type: number
+// // "33abc" => NaN(not a number) // type: number
+// // true => 1; false => 0 // type: number 
+// // null => 0 // type: object
+// // undefined => NaN // type: undefined
 
-let stringNumber = String(someNumber)
+// let isLoggedIn = 1
 
-console.log(typeof stringNumber)
+// let booleanLoggedIn = Boolean(isLoggedIn)
 
-console.log(stringNumber)
+// console.log(typeof booleanLoggedIn) 
+
+// console.log(booleanLoggedIn)
+
+// // 1 or non zero number => true; 0 => false // type: boolean
+// // "" => false // type: boolean
+// // "Anil" => true // type:boolean 
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+
+// console.log(typeof stringNumber)
+
+// console.log(stringNumber)
+
+
+// ******************** Operations *******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(4/2);
+// console.log(5%2);
+
+// let str1 = "Hello"
+// let str2 = " Anil"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2) // 12
+// console.log(1 + "2") // 12
+// console.log("1" + 2 + 2) // 122
+// console.log(1 + 2 + "3") // 33
+
+// Copy the link to know about it => "https://tc39.es/ecma262/#sec-type-conversion"
+
+// console.log(3 + 4 * 5 % 3) // 5 
+
+// console.log(+true); // 1
+// console.log(true+); // error
+// console.log(+"") // 0 => "" will be converted as false: 0
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+
+// let gameCounter = 100;
+// gameCounter++; // ++gameCounter
+// console.log(gameCounter);
+// Prefix and Postfix => "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment"
+
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// // x2 is 4n; y2 is 3n
+// console.log(x2,y2);
+
+// Postfix increment => assigns the value first, then increments the actual value.
+// Prefix increment => FIrst increments the value and then assign it.
