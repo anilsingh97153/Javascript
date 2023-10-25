1ab676215bf860a78b88ef0378424244de7d43ea
Anil Singh <52264327+anilsuyal@users.noreply.github.com>
2023-10-25 20:04:18 +0000

    While and do while loops done.

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,16 @@
+// while loop 
+
+// let index = 1;
+// while (index <= 10) {
+//     console.log(`${index}`);
+//     index++;
+// }
+
+// do while loop => used rarely
+
+let score = 1;
+do {
+    console.log(`${score}`);
+    score++;
+} while(score <= 10)
+
