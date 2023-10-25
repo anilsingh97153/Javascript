c59dafaf913f6bb7974f8cd8361cebdf089a4ee1
Anil Singh <52264327+anilsuyal@users.noreply.github.com>
2023-10-25 19:13:23 +0000

    Switch statement done.

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,43 @@
+// The switch statement evaluates an expression, matching the expression's value against a series of case clauses, and executes statements after the first case clause with a matching value, until a break statement is encountered. The default clause of a switch statement will be jumped to if no case matches the expression's value.
+
+// shit + alt + down arrow => make a copy of selected lines to the below lines.
+
+// const month = 3
+
+// switch(month) {
+//     case 1:
+//         console.log("January");
+//         break; 
+//     case 2:
+//         console.log("February");
+//         break; 
+//     case 3:
+//         console.log("March");
+//         break; 
+//     case 4:
+//         console.log("April");
+//         break; 
+    
+//     default: 
+//         console.log("No match!");    
+// }
+
+// const nextMonth = "march"
+
+// switch(nextMonth) {
+//     case "january":
+//         console.log("January");
+//         break; 
+//     case "february":
+//         console.log("February");
+//         break; 
+//     case "march":
+//         console.log("March");
+//         break; 
+//     case "april":
+//         console.log("April");
+//         break; 
+    
+//     default: 
+//         console.log("No match!");    
+// }
