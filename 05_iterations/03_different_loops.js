770e977f1527fcacea2d7253035a4cbb0ff4495b
Anil Singh <52264327+anilsuyal@users.noreply.github.com>
2023-10-26 15:27:26 +0000

    Various types of loops done.

---
+++ 05_iterations/03_different_loops.js
@@ -0,0 +1,37 @@
+// for of 
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// for (const num of numbers) {
+//     console.log(`${num}`);
+// }
+
+// const greetings = " Hello world!";
+// for (const greet of greetings) {
+//     console.log(`Char: ${greet}`);
+// }
+
+// Maps => contains unique key value pairs
+
+// const map = new Map()
+// map.set('Name', "Anil");
+// map.set('age', "22");
+
+// console.log(map);
+
+// // using for of loop on above map
+// // [key, value] => destructuring of map
+// for (const [key, val] of map) {
+//     consol e.log(`${key}:- ${val}`); 
+// }
+
+// for of loop on object => which is not iteratable
+
+// const myObject = {
+//     'game1': 'NFS',
+//     'game2': 'Spiderman'
+// }
+
+// for (const [key, val] of myObject) {
+//     console.log(`${key}:- ${val}`);
+// }
