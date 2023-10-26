665acba0f3fe1b4ba0c11c499d44990727b6861e
Anil Singh <52264327+anilsuyal@users.noreply.github.com>
2023-10-26 19:04:21 +0000

    Filter method done.

---
+++ 05_iterations/06_filter.js
@@ -0,0 +1,65 @@
+// Filter 
+// const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// // forEach doesn't return anything, even if you explicitly return.
+// const values = coding.forEach((item) => {
+//     console.log(item);
+//     return item;
+// })
+
+// console.log(values);
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// filter takes a callback function followed by a condition which defines the nature of output, like here in this case it is all the values which are greater than 4 (num > 4). This thing is pretty handy when we want to return values based on specific conditions from a loop.
+
+// const newNums = myNums.filter( (num) => num > 5) 
+
+// console.log(newNums);
+
+// above code in diff way
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4; 
+// })
+
+// console.log(newNums);
+
+
+// Implementing the same thing thorugh forEach loop
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if(num > 4) {
+//         newNums.push(num);
+//     }
+// } )
+
+// console.log(newNums);
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+let userBooks = books.filter( (book) => book.genre === 'History' )
+
+console.log(userBooks);
+
+userBooks = books.filter( (book) => { return book.publish >= 2000})
+
+console.log(userBooks);
+
+userBooks = books.filter( (book) => {
+    return book.publish >= 1995 && book.genre === "History"; 
+})
+
+console.log(userBooks);
